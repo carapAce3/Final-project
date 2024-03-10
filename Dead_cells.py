c8a40e7512e1183731d199ad92ce4ca4cbbb0805
carapAce3 <143933759+carapAce3@users.noreply.github.com>
2024-03-10 13:30:46 +0200

    Add files via upload

--- Dead_cells.py
+++ Dead_cells.py
@@ -17,26 +17,35 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-
+jump = False
+gravity = -10
 # клас-спадкоємець для спрайту-гравця (керується стрілками)    
 class Player(GameSprite):
     def move(self):
         keys = key.get_pressed()
-        if keys[K_d] and self.rect.x > 5:
+        if keys[K_d] and self.rect.x < win_width-50:
             self.rect.x += self.speed
-        if keys[K_a] and self.rect.x < win_height - 80:
+        if keys[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
         if keys[K_SPACE] and self.rect.y > 5:
-            self.rect.y -= self.speed
+                global gravity 
+                self.rect.y += gravity
+
+            
 #ігрова сцена:
 back = (200, 255, 255)  #колір фону (background)
 win_width = 1200
 win_height = 600
 window = display.set_mode((win_width, win_height))
-window.fill(back)
+back = image.load('map.jpg')
+#window.fill(back)
  
-player1 = Player('guy.png',50,100,10,40,80)
-
+guy = Player('guy.png',50,390,10,40,80)
+sword = Player('sword.png',50,100,10,40,80)
+bow = Player('bow.png',50,100,10,40,80)
+platform = GameSprite('platform.png', 30,460,10,200,100)
+platforms = []
+platforms.append(platform)
 
 
 
@@ -53,12 +62,23 @@ while game:
             game = False
         if e.type == MOUSEBUTTONDOWN:
             if e.button == 1:
-                player1.rect.x = e.pos[0] - 20
-                player1.rect.y = e.pos[1] - 20
-    window.fill(back)
-    player1.reset()
-    player1.move()
-   
+                print('')
+                
+    #window.fill(back)
+    window.blit(back,(0,0))
+    guy.reset()
+    platform.reset()
+    guy.move()
+    if guy.rect.y <= 390:
+        guy.rect.y += 3
+
+
+    
+
+
+    #if sprite.collide_rect(guy,platform):
+        #guy.rect.y += 1
+        #guy.rect.y -= 1
    
     display.update()
     clock.tick(60)
